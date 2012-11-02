788bfe353e4b3524ecaea36acc571d38074e9103
Tomasz Dziurko <tdziurko@gmail.com>
2012-11-02 15:11:25 +0100

    Missing service file

---
+++ sml-bootstrap/bootstrap-service/src/main/scala/pl/softwaremill/bootstrap/service/EntryService.scala
@@ -0,0 +1,33 @@
+package pl.softwaremill.bootstrap.service
+
+import pl.softwaremill.bootstrap.domain.Entry
+import pl.softwaremill.bootstrap.dao.EntryDAO
+
+object EntryService {
+
+  def loadAll = {
+    EntryDAO.loadAll
+  }
+
+  def count(): Long = {
+    EntryDAO.count()
+  }
+
+  def add(entry: Entry) {
+    EntryDAO.add(entry)
+  }
+
+  def remove(entryId: Int) {
+    EntryDAO.remove(entryId)
+  }
+
+  def load(entryId: Int) {
+    EntryDAO.load(entryId)
+  }
+
+  def update(entry: Entry) {
+    EntryDAO.update(entry)
+  }
+
+
+}
