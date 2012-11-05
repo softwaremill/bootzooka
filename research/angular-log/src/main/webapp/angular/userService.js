33ab53acfa93e63684fcd8cff80ceedc98540dae
lukaszlenart <lukasz.lenart@gmail.com>
2012-11-05 14:23:10 +0100

    Moves demo projects to dedicated repo next-research

--- research/angular-log/src/main/webapp/angular/userService.js
+++
@@ -1,8 +0,0 @@
-angular.module('userService', ['ngResource']).
-    factory('UserService', function($resource) {
-        var UserService = $resource('/users', { },
-            { }
-        );
-
-        return UserService;
-    });
