c55af0a7dfea35dc50f125df85db650aac966add
lukaszlenart <lukasz.lenart@gmail.com>
2012-11-07 16:02:50 +0100

    Adds token handling logic

--- bootstrap-rest/src/main/scala/pl/softwaremill/bootstrap/auth/User.scala
+++ bootstrap-rest/src/main/scala/pl/softwaremill/bootstrap/auth/User.scala
@@ -1,8 +1,18 @@
 package pl.softwaremill.bootstrap.auth
 
-case class User(login: String, password: String)
+import pl.softwaremill.bootstrap.common.Utils
+
+case class User(login: String, password: String) {
+
+    def token: String = Utils.md5(login)
+
+}
 
 object Users {
+
+    def validateToken(token: String) = {
+        list.find(_.token == token)
+    }
 
     val list = List(
         User("admin", "admin"),
