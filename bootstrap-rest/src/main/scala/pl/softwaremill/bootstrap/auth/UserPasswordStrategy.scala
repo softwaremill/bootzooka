65c28b3f0b048cc7e56f6fd2d9ec3247700d05db
lukaszlenart <lukasz.lenart@gmail.com>
2012-11-07 13:55:07 +0100

    Adds new strategy to use with Scentry authentication

---
+++ bootstrap-rest/src/main/scala/pl/softwaremill/bootstrap/auth/UserPasswordStrategy.scala
@@ -0,0 +1,27 @@
+
+package pl.softwaremill.bootstrap.auth
+
+import org.scalatra.ScalatraBase
+import org.scalatra.auth.ScentryStrategy
+
+object UserPasswordStrategy {
+
+  class UserPasswordStrategy(protected val app: ScalatraBase, login: String, password: String) extends ScentryStrategy[User] {
+
+    override def isValid = {
+      !login.isEmpty && !password.isEmpty
+    }
+
+    override def authenticate() = {
+      val userOpt: Option[User] = Users.list.find{user =>
+        user.login == login && user.password == password
+      }
+      userOpt match {
+        case Some(user) => Option(user)
+        case _ => None
+      }
+    }
+
+  }
+
+}
