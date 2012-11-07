840b802d8e9d6de7239f236869cd272c346f1bdd
lukaszlenart <lukasz.lenart@gmail.com>
2012-11-07 16:02:32 +0100

    Adds rememberMe method and default implementations for login and password

--- bootstrap-rest/src/main/scala/pl/softwaremill/bootstrap/auth/AuthenticationSupport.scala
+++ bootstrap-rest/src/main/scala/pl/softwaremill/bootstrap/auth/AuthenticationSupport.scala
@@ -7,46 +7,51 @@ import pl.softwaremill.bootstrap.common.JsonWrapper
 
 trait AuthenticationSupport extends ScentrySupport[User] {
 
-  self: ScalatraBase =>
+    self: ScalatraBase =>
 
-  override protected def registerAuthStrategies {
-    scentry.register("UserPassword", app => new UserPasswordStrategy(app, login, password))
-  }
-
-  protected def fromSession = {
-    case id: String => {
-      val userOpt: Option[User] = Users.list.find(_.login == id)
-      userOpt match {
-        case Some(u) => u
-        case _ => null
-      }
+    override protected def registerAuthStrategies {
+        scentry.register("UserPassword", app => new UserPasswordStrategy(app, login, password))
     }
-  }
 
-  protected def toSession = {
-    case usr: User => usr.login
-  }
-
-  protected def scentryConfig = (new ScentryConfig {}).asInstanceOf[ScentryConfiguration]
-
-  def login: String
-  def password: String
-
-  post() {
-    println("authenticate")
-    val userOpt: Option[User] = authenticate()
-    userOpt match {
-      case Some(user) =>
-        println("Matched user %s".format(user))
-        JsonWrapper(user)
-      case _ =>
-        halt(401, "Invalid login and/or password")
+    protected def fromSession = {
+        case id: String => {
+            val userOpt: Option[User] = Users.list.find(_.login == id)
+            userOpt match {
+                case Some(u) => u
+                case _ => null
+            }
+        }
     }
-  }
 
-  get("/logout") {
-    println("logout")
-    logOut()
-  }
+    protected def toSession = {
+        case usr: User => usr.login
+    }
+
+    protected def scentryConfig = (new ScentryConfig {}).asInstanceOf[ScentryConfiguration]
+
+    /**
+     * Override to configure login process, must be only done on Login form
+     */
+    protected def login: String = ""
+
+    protected def password: String = ""
+
+    protected def rememberMe: Boolean = false
+
+    post() {
+        val userOpt: Option[User] = authenticate()
+        userOpt match {
+            case Some(user) =>
+                println("Matched user %s".format(user))
+                JsonWrapper(user)
+            case _ =>
+                halt(401, "Invalid login and/or password")
+        }
+    }
+
+    get("/logout") {
+        println("logout")
+        logOut()
+    }
 
 }
