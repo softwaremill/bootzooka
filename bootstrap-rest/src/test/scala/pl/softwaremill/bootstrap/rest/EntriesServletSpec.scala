6cb960b5e2cb2dcb98c1e375dc5dc3bc39f1be6a
lukaszlenart <lukasz.lenart@gmail.com>
2012-11-09 12:11:52 +0100

    Adds additional tests with second spec defined

--- bootstrap-rest/src/test/scala/pl/softwaremill/bootstrap/rest/EntriesServletSpec.scala
+++ bootstrap-rest/src/test/scala/pl/softwaremill/bootstrap/rest/EntriesServletSpec.scala
@@ -5,14 +5,32 @@ import org.scalatra.test.specs2._
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class EntriesServletSpec extends ScalatraSpec {
 
-    def is = "GET / on EntriesServlet" ^
-        "should return status 200" ! root200 ^
-        end
+    def is =
+        "GET / on EntriesServlet" ^
+            "should return status 200" ! root200 ^
+            "should return content-type application/json" ! contentJson ^
+            "should return JSON entries" ! jsonEntries add
+        "GET /count on EntriesServlet" ^
+            "should return number of entries" ! countEntries
+
+    end
 
     addServlet(classOf[EntriesServlet], "/*")
 
     def root200 = get("/") {
         status must_== 200
+    }
+
+    def contentJson = get("/") {
+        header.get("Content-Type").get contains "application/json"
+    }
+
+    def jsonEntries = get("/") {
+        body must startWith("[{") and endWith("}]")
+    }
+
+    def countEntries = get("/count") {
+        body must contain("{\"value\":3}")
     }
 
 }
