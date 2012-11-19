ad23fa3d48dc36beae8f9b2821a814e183a11929
lukaszlenart <lukasz.lenart@gmail.com>
2012-11-19 12:05:47 +0100

    Applies code reformat

--- project/Build.scala
+++ project/Build.scala
@@ -2,89 +2,93 @@ import sbt._
 import Keys._
 
 object Resolvers {
-  val bootstrapResolvers = Seq(
-    "Sonatype releases" at "http://oss.sonatype.org/content/repositories/releases/",
-    "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-    "SotwareMill Public Releases" at "http://tools.softwaremill.pl/nexus/content/repositories/releases/",
-    "JBoss Releases" at "https://repository.jboss.org/nexus/content/groups/public",
-    "Twitter Maven" at "http://maven.twttr.com")
+
+    val bootstrapResolvers = Seq(
+        "Sonatype releases" at "http://oss.sonatype.org/content/repositories/releases/",
+        "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+        "SotwareMill Public Releases" at "http://tools.softwaremill.pl/nexus/content/repositories/releases/",
+        "JBoss Releases" at "https://repository.jboss.org/nexus/content/groups/public",
+        "Twitter Maven" at "http://maven.twttr.com")
+
 }
 
 object BuildSettings {
 
-  import Resolvers._
+    import Resolvers._
 
-  val buildSettings = Defaults.defaultSettings ++ Seq(
-    organization := "pl.softwaremill",
-    version := "0.0.1-SNAPSHOT",
-    scalaVersion := "2.9.2",
+    val buildSettings = Defaults.defaultSettings ++ Seq(
+        organization := "pl.softwaremill",
+        version := "0.0.1-SNAPSHOT",
+        scalaVersion := "2.9.2",
 
-    resolvers := bootstrapResolvers,
-    scalacOptions += "-unchecked",
-    classpathTypes ~= (_ + "orbit")
-  )
+        resolvers := bootstrapResolvers,
+        scalacOptions += "-unchecked",
+        classpathTypes ~= (_ + "orbit")
+    )
+
 }
 
 object Dependencies {
 
-  val scalatraVersion = "2.2.0-RC1"
+    val scalatraVersion = "2.2.0-RC1"
 
-  val logback = "ch.qos.logback" % "logback-classic" % "1.0.6"
-  val scalatra = "org.scalatra" % "scalatra" % scalatraVersion
-  val scalatraSpec2 = "org.scalatra" % "scalatra-specs2" % scalatraVersion % "test"
-  val scalatraJson = "org.scalatra" % "scalatra-json" % scalatraVersion
-  val json4s = "org.json4s"   %% "json4s-jackson" % "3.0.0"
-  val scalatraAuth = "org.scalatra" % "scalatra-auth" % scalatraVersion
-  val scalatraStack = Seq(scalatra, scalatraSpec2, scalatraJson, json4s, logback, scalatraAuth)
+    val logback = "ch.qos.logback" % "logback-classic" % "1.0.6"
+    val scalatra = "org.scalatra" % "scalatra" % scalatraVersion
+    val scalatraSpec2 = "org.scalatra" % "scalatra-specs2" % scalatraVersion % "test"
+    val scalatraJson = "org.scalatra" % "scalatra-json" % scalatraVersion
+    val json4s = "org.json4s" %% "json4s-jackson" % "3.0.0"
+    val scalatraAuth = "org.scalatra" % "scalatra-auth" % scalatraVersion
+    val scalatraStack = Seq(scalatra, scalatraSpec2, scalatraJson, json4s, logback, scalatraAuth)
 
-  val jettyOrbit = "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+    val jettyOrbit = "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+
 }
 
 object SmlBootstrapBuild extends Build {
 
-  import Dependencies._
-  import BuildSettings._
+    import Dependencies._
+    import BuildSettings._
 
-  lazy val parent: Project = Project(
-    "bootstrap-root",
-    file("."),
-    settings = buildSettings
-  ) aggregate(common, domain, dao, service, rest, ui)
+    lazy val parent: Project = Project(
+        "bootstrap-root",
+        file("."),
+        settings = buildSettings
+    ) aggregate(common, domain, dao, service, rest, ui)
 
-  lazy val common: Project = Project(
-    "bootstrap-common",
-    file("bootstrap-common"),
-    settings = buildSettings
-  )
+    lazy val common: Project = Project(
+        "bootstrap-common",
+        file("bootstrap-common"),
+        settings = buildSettings
+    )
 
-  lazy val domain: Project = Project(
-    "bootstrap-domain",
-    file("bootstrap-domain"),
-    settings = buildSettings
-  ) dependsOn(common)
+    lazy val domain: Project = Project(
+        "bootstrap-domain",
+        file("bootstrap-domain"),
+        settings = buildSettings
+    ) dependsOn (common)
 
-  lazy val dao: Project = Project(
-    "bootstrap-dao",
-    file("bootstrap-dao"),
-    settings = buildSettings
-  ) dependsOn (domain, common)
+    lazy val dao: Project = Project(
+        "bootstrap-dao",
+        file("bootstrap-dao"),
+        settings = buildSettings
+    ) dependsOn(domain, common)
 
-  lazy val service: Project = Project(
-    "bootstrap-service",
-    file("bootstrap-service"),
-    settings = buildSettings
-  ) dependsOn (domain, dao, common)
+    lazy val service: Project = Project(
+        "bootstrap-service",
+        file("bootstrap-service"),
+        settings = buildSettings
+    ) dependsOn(domain, dao, common)
 
-  lazy val rest: Project = Project(
-    "bootstrap-rest",
-    file("bootstrap-rest"),
-    settings = buildSettings ++ Seq(libraryDependencies := scalatraStack ++ Seq(jettyOrbit))
-  ) dependsOn (service, domain, common)
+    lazy val rest: Project = Project(
+        "bootstrap-rest",
+        file("bootstrap-rest"),
+        settings = buildSettings ++ Seq(libraryDependencies := scalatraStack ++ Seq(jettyOrbit))
+    ) dependsOn(service, domain, common)
 
-  lazy val ui: Project = Project(
-    "bootstrap-ui",
-    file("bootstrap-ui"),
-    settings = buildSettings ++ Seq(libraryDependencies := Seq(jettyOrbit))
-  ) dependsOn (rest)
+    lazy val ui: Project = Project(
+        "bootstrap-ui",
+        file("bootstrap-ui"),
+        settings = buildSettings ++ Seq(libraryDependencies := Seq(jettyOrbit))
+    ) dependsOn (rest)
 
 }
