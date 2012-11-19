374a26b419dad063fe56bcd97c07978d564d7914
lukaszlenart <lukasz.lenart@gmail.com>
2012-11-19 12:11:18 +0100

    Adds ScalaTest dependency

--- project/Build.scala
+++ project/Build.scala
@@ -39,6 +39,7 @@ object Dependencies {
     val json4s = "org.json4s" %% "json4s-jackson" % "3.0.0"
     val scalatraAuth = "org.scalatra" % "scalatra-auth" % scalatraVersion
     val scalatraStack = Seq(scalatra, scalatraSpec2, scalatraJson, json4s, logback, scalatraAuth)
+    val scalaTest = "org.scalatest" %% "scalatest" % "1.8" % "test"
 
     val jettyOrbit = "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
 
