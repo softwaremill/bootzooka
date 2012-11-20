cea4ff72c10bd09dac719c8b40eab5eb44fb9209
adamw <adam@warski.org>
2012-11-20 20:19:53 +0100

    Adding jasmine settings

--- project/Build.scala
+++ project/Build.scala
@@ -92,7 +92,15 @@ object SmlBootstrapBuild extends Build {
   lazy val ui: Project = Project(
     "bootstrap-ui",
     file("bootstrap-ui"),
-    settings = buildSettings ++ jasmineSettings ++ Seq(libraryDependencies ++= Seq(jettyOrbit))
+    settings = buildSettings ++ jasmineSettings ++ Seq(
+      libraryDependencies ++= Seq(jettyOrbit),
+      appJsDir <+= sourceDirectory { src => src / "main" / "webapp" / "app" },
+      appJsLibDir <+= sourceDirectory { src => src / "main" / "webapp" / "assets" / "js" },
+      jasmineTestDir <+= sourceDirectory { src => src / "test" / "unit" },
+      jasmineConfFile <+= sourceDirectory { src => src / "test" / "webapp" / "static" / "js" / "test.dependencies.js" },
+      jasmineRequireJsFile <+= sourceDirectory { src => src / "main" / "webapp" / "static" / "js" / "lib" / "require" / "require-2.0.6.js" },
+      jasmineRequireConfFile <+= sourceDirectory { src => src / "test" / "webapp" / "static" / "js" / "require.conf.js" },
+      (test in Test) <<= (test in Test) dependsOn (jasmine))
   ) dependsOn (rest)
 
 }
