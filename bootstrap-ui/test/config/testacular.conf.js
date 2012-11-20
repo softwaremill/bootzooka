b337bd2614d88f1ca3ae1862af90786c575c09f7
Tomasz Dziurko <tdziurko@gmail.com>
2012-11-20 13:42:14 +0100

    First working unit test for EntriesController

---
+++ bootstrap-ui/test/config/testacular.conf.js
@@ -0,0 +1,20 @@
+basePath = '../';
+
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  '../src/main/webapp/assets/js/angular.js',
+  '../src/main/webapp/assets/js/angular-*.js',
+  'lib/angular/angular-mocks.js',
+  '../src/main/webapp/app/*.js',
+  'unit/**/*.js'
+];
+
+autoWatch = true;
+
+browsers = ['Chrome'];
+
+junitReporter = {
+  outputFile: 'test_out/unit.xml',
+  suite: 'unit'
+};
---
+++ bootstrap-ui/test/unit/entriesControllerSpec.js
@@ -0,0 +1,111 @@
+'use strict';
+
+describe("Entries Controller", function () {
+
+    beforeEach(module('entriesService', 'entriesCounterService', 'authService'));
+
+    afterEach(inject(function(_$httpBackend_) {
+        _$httpBackend_.verifyNoOutstandingExpectation();
+        _$httpBackend_.verifyNoOutstandingRequest();
+    }));
+
+    describe('with not-empty data response', function () {
+        var scope, $httpBackend, ctrl;
+
+        beforeEach(inject(function (_$httpBackend_, $rootScope, $routeParams, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.whenGET('/rest/entries/count').respond('{"value":2}');
+            $httpBackend.whenGET('rest/entries').respond('[{"id":1,"author":"Jan Kowalski","text":"Short message"},' +
+                '{"id":2,"author":"Piotr Nowak","text":"Very long message"}]');
+
+            scope = $rootScope.$new();
+            ctrl = $controller(EntriesController, {$scope:scope});
+
+            $rootScope.loggedUser = {
+                login: "Jan Kowalski"
+            }
+
+            $httpBackend.flush();
+        }));
+
+        it('Should initialize entry text as empty', function () {
+            expect(scope.entryText).toBe('');
+        });
+
+        it('Should have size and logs defined', function () {
+            expect(scope.logs).toBeDefined();
+            expect(scope.size).toBeDefined();
+        });
+
+        it('Should have size set to two', function () {
+            expect(scope.size).toBe(2);
+        });
+
+        it("NoEntries should return false", function () {
+            expect(scope.noEntries()).toBe(false);
+        });
+
+        it('Should have user logged', function () {
+            expect(scope.isLogged()).toBe(true);
+        });
+
+        it("Should mark user as an owner of first entry", function () {
+            expect(scope.isOwnerOf(scope.logs[0])).toBe(true);
+        });
+
+        it("Should not mark user as an owner of second entry", function () {
+            expect(scope.isOwnerOf(scope.logs[1])).toBe(false);
+        });
+    });
+
+
+    describe('with empty data response', function () {
+        var scope, $httpBackend, ctrl;
+
+        beforeEach(inject(function (_$httpBackend_, $rootScope, $routeParams, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.whenGET('/rest/entries/count').respond('{"value":0}');
+            $httpBackend.whenGET('rest/entries').respond('[{}]');
+
+            scope = $rootScope.$new();
+            ctrl = $controller(EntriesController, {$scope:scope});
+
+            $rootScope.loggedUser = {
+                login: "Jan Kowalski"
+            }
+
+            $httpBackend.flush();
+        }));
+
+        it('Should have size set to zero', function () {
+            expect(scope.size).toBe(0);
+        });
+
+        it("NoEntries should return true", function () {
+            expect(scope.noEntries()).toBe(true);
+        });
+    });
+
+    describe('without logged user', function () {
+        var scope, $httpBackend, ctrl;
+
+        beforeEach(inject(function (_$httpBackend_, $rootScope, $routeParams, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.whenGET('/rest/entries/count').respond('{"value":2}');
+            $httpBackend.whenGET('rest/entries').respond('[{"id":1,"author":"Jan Kowalski","text":"Short message"},' +
+                '{"id":2,"author":"Piotr Nowak","text":"Very long message"}]');
+
+            scope = $rootScope.$new();
+            ctrl = $controller(EntriesController, {$scope: scope});
+
+            $httpBackend.flush();
+        }));
+
+        it('Should have user not logged', function () {
+            expect(scope.isLogged()).toBe(false);
+        });
+    });
+
+
+
+});
