bd093f88ce95021e9a028c568a9f4b8c327048c2
adamw <adam@warski.org>
2012-11-20 19:14:58 +0100

    Defining all plugins in one place

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -1,8 +1,17 @@
 import sbt._
+import Keys._
 
 object Plugins extends Build {
 
-  lazy val plugins = Project("plugins", file(".")).dependsOn(
+  addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.1.0")
+
+  lazy val plugins = Project(
+    "plugins",
+    file("."),
+    settings = Defaults.defaultSettings ++ Seq(
+      libraryDependencies += ("com.github.siasia" %% "xsbt-web-plugin" % "0.12.0-0.2.11.1"),
+      addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.1.0")))
+      .dependsOn(
     // TeamCity reporting, see: https://github.com/guardian/sbt-teamcity-test-reporting-plugin
     uri("git://github.com/guardian/sbt-teamcity-test-reporting-plugin.git#1.2"),
 
