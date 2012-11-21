39584d396c45af8b6d91b461019f07029a8fc26d
Tomasz Dziurko <tdziurko@gmail.com>
2012-11-21 14:28:02 +0100

    Added missing test.dependencies.js file

---
+++ bootstrap-ui/src/test/unit/test.dependencies.js
@@ -0,0 +1,21 @@
+// Libraries
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "jquery-1.8.2-min.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-1.1.0.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-resource-1.1.0.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-sanitize-1.1.0.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "bootstrap-2.2.0.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "require/require-2.0.6.js");
+
+
+// Testing libraries
+EnvJasmine.loadGlobal(EnvJasmine.testDir + "require.conf.js");
+EnvJasmine.loadGlobal(EnvJasmine.testDir + "../lib/angular/angular-mocks.js");
+
+
+// Application
+EnvJasmine.loadGlobal(EnvJasmine.rootDir + "application.js");
+EnvJasmine.loadGlobal(EnvJasmine.rootDir + "authService.js");
+EnvJasmine.loadGlobal(EnvJasmine.rootDir + "controllers.js");
+EnvJasmine.loadGlobal(EnvJasmine.rootDir + "effects.js");
+EnvJasmine.loadGlobal(EnvJasmine.rootDir + "entriesService.js");
+EnvJasmine.loadGlobal(EnvJasmine.rootDir + "utilService.js");
