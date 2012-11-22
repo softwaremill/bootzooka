63aa562bb8adf2993fb0f532a8c6336f65a2b180
Tomasz Dziurko <tdziurko@gmail.com>
2012-11-22 08:42:27 +0100

    Moved require JS fie to test package

--- bootstrap-ui/src/test/unit/test.dependencies.js
+++ bootstrap-ui/src/test/unit/test.dependencies.js
@@ -4,10 +4,10 @@ EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-1.1.0.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-resource-1.1.0.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-sanitize-1.1.0.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "bootstrap-2.2.0.js");
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "require/require-2.0.6.js");
 
 
 // Testing libraries
+EnvJasmine.loadGlobal(EnvJasmine.testDir + "../lib/require/require-2.0.6.js");
 EnvJasmine.loadGlobal(EnvJasmine.testDir + "require.conf.js");
 EnvJasmine.loadGlobal(EnvJasmine.testDir + "../lib/angular/angular-mocks.js");
 
