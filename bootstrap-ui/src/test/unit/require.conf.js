c7531dce0f9ad0d98787ff50b617c88b5ddfe8a7
Tomasz Dziurko <tdziurko@gmail.com>
2012-11-22 08:43:00 +0100

    Removed fixme mark taken from example jasmine-sbt confguration.

--- bootstrap-ui/src/test/unit/require.conf.js
+++ bootstrap-ui/src/test/unit/require.conf.js
@@ -1,12 +1,6 @@
 require.config({
     baseUrl: EnvJasmine.rootDir,
     paths: {
-        specs:      EnvJasmine.specsDir,
-
-        // Libraries
-        // FIXME: we don't want to depend on jQuery necessarily, but for
-        // now the sbt plugin requires it
-        jquery:     '../assets/jquery-1.8.2-min'
-
+        specs:      EnvJasmine.specsDir
     }
 });
