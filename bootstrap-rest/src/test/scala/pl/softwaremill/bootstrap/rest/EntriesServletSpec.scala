5745d7ba18055990ef6f5ad79d93a0eae975e49f
lukaszlenart <lukasz.lenart@gmail.com>
2012-11-27 12:13:07 +0100

    Uses wrapping method to setup on each test a new servlet with fresh mocks

--- bootstrap-rest/src/test/scala/pl/softwaremill/bootstrap/rest/EntriesServletSpec.scala
+++ bootstrap-rest/src/test/scala/pl/softwaremill/bootstrap/rest/EntriesServletSpec.scala
@@ -4,6 +4,7 @@ import org.scalatra.test.specs2._
 import pl.softwaremill.bootstrap.service.{UserService, EntryService}
 import org.specs2.mock.Mockito
 import pl.softwaremill.bootstrap.domain.Entry
+import org.specs2.matcher.MatchResult
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class EntriesServletSpec extends ScalatraSpec with Mockito {
@@ -22,40 +23,58 @@ class EntriesServletSpec extends ScalatraSpec with Mockito {
 
   end
 
-  def mockedService: EntryService = {
-    val m = mock[EntryService]
-    m.count() returns 4
-    m.loadAll returns List(Entry(1, "Jas Kowalski", "Important message"))
-    m
+  def onServletWithMocks(test:(EntryService, UserService) => MatchResult[Any]): MatchResult[Any] = {
+    val userService = mock[UserService]
+
+    val entryService = mock[EntryService]
+    entryService.count() returns 4
+    entryService.loadAll returns List(Entry(1, "Jas Kowalski", "Important message"))
+
+    val servlet: EntriesServlet = new EntriesServlet(entryService, userService)
+    addServlet(servlet, "/*")
+
+    test(entryService, userService)
   }
 
-  def userService = mock[UserService]
-
-  addServlet(new EntriesServlet(mockedService, userService), "/*")
-
-  def root200 = get("/") {
-    status must_== 200
+  def root200 = onServletWithMocks { (entryService, userService) =>
+    get("/") {
+      status === 200
+      there was one(userService).loadAll
+      there was no(userService).load(0)
+    }
   }
 
-  def contentJson = get("/") {
-    header("Content-Type") contains "application/json"
+  def contentJson = onServletWithMocks { (entryService, userService) =>
+    get("/") {
+      header("Content-Type") contains "application/json"
+      there was one(entryService).loadAll
+      there was no(entryService).load(0)
+    }
   }
 
   def jsonEntries = get("/") {
     body must contain("[{\"id\":1,\"author\":\"Jas Kowalski\",\"text\":\"Important message\"}]")
   }
 
-  def countEntries = get("/count") {
-    body must contain("{\"value\":4}")
+  def countEntries = onServletWithMocks { (entryService, userService) =>
+    get("/count") {
+      body must contain("{\"value\":4}")
+      there was one(entryService).count()
+    }
   }
 
-  def tryUpdateExistingEntry = post("/", "anything") {
-    status must_== 401
+  def tryUpdateExistingEntry = onServletWithMocks { (entryService, userService) =>
+    post("/", "anything") {
+      status === 401
+      there was noCallsTo(entryService)
+    }
   }
 
-  def tryCreateNewEntry = put("/", "anything") {
-    status must_== 401
+  def tryCreateNewEntry =  onServletWithMocks { (entryService, userService) =>
+    put("/", "anything") {
+      status === 401
+      there was noCallsTo(entryService)
+    }
   }
-
 
 }
