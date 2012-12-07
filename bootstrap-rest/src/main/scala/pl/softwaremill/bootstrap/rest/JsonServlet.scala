d920646b197de744f8e650cb97922d488794bd3b
lukaszlenart <lukasz.lenart@gmail.com>
2012-12-07 13:16:17 +0100

    Adds header to prevent caching JSON - solves bug for IE8/9

--- bootstrap-rest/src/main/scala/pl/softwaremill/bootstrap/rest/JsonServlet.scala
+++ bootstrap-rest/src/main/scala/pl/softwaremill/bootstrap/rest/JsonServlet.scala
@@ -5,14 +5,20 @@ import org.scalatra.json.{JValueResult, JacksonJsonSupport}
 import org.json4s._
 import java.io.Writer
 import org.apache.commons.lang3.StringEscapeUtils._
+import org.json4s.{DefaultFormats, Formats}
+import javax.servlet.http.HttpServletResponse
+import java.util.Date
 
 class JsonServlet extends ScalatraServlet with JacksonJsonSupport with JValueResult {
 
-    protected implicit val jsonFormats: Formats = DefaultFormats
+  protected implicit val jsonFormats: Formats = DefaultFormats
 
-    before() {
-      contentType = formats("json")
-    }
+  val Expire = new Date().toString
+
+  before() {
+    contentType = formats("json")
+    applyNoCache(response)
+  }
 
   override def writeJson(json: JValue, writer: Writer) {
     (json \ "notEscapedData") match {
@@ -29,6 +35,13 @@ class JsonServlet extends ScalatraServlet with JacksonJsonSupport with JValueRes
         mapper.writeValue(writer, json \ "notEscapedData")
       }
     }
+  }
+
+  def applyNoCache(response: HttpServletResponse) {
+    response.addHeader("Expires", Expire)
+    response.addHeader("Last-Modified", Expire)
+    response.addHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+    response.addHeader("Pragma", "no-cache")
   }
 
 }
