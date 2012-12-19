05354ab54ed48ccc7a8a80845966664b243f801a
Tomasz Dziurko <tdziurko@gmail.com>
2012-12-19 11:35:41 +0100

    Additional tests for MongoEntryDAO, solves #9

--- bootstrap-dao/src/test/scala/pl/softwaremill/bootstrap/dao/MongoEntryDAOSpec.scala
+++ bootstrap-dao/src/test/scala/pl/softwaremill/bootstrap/dao/MongoEntryDAOSpec.scala
@@ -22,6 +22,20 @@ class MongoEntryDAOSpec extends SpecificationWithMongo {
       assert(entryDAO.countItems() === 3)
     }
 
+    "load all entries" in {
+      assert(entryDAO.loadAll.size === 3)
+    }
+
+    "load single entry" in {
+      val entryOpt: Option[Entry] = entryDAO.load("1" * 24)
+      assert(entryOpt.isDefined === true)
+    }
+
+    "not find non existing entry" in {
+      val entryOpt: Option[Entry] = entryDAO.load("19" * 12)
+      assert(entryOpt.isDefined === false)
+    }
+
     "add new entry" in {
       // Given
       val numberOfEntries = entryDAO.countItems()
@@ -43,6 +57,24 @@ class MongoEntryDAOSpec extends SpecificationWithMongo {
 
       // Then
       assert(entryDAO.countItems() - numberOfEntries === -1)
+    }
+
+    "update existing entry" in {
+      // Given
+      val entryOpt: Option[Entry] = entryDAO.load("1" * 24)
+      val message: String = "Updated message"
+
+      // When
+      entryOpt.foreach( e => entryDAO.update(e._id.toString, message))
+
+      // Then
+      val updatedEntryOpt: Option[Entry] = entryDAO.load("1" * 24)
+      updatedEntryOpt match {
+        case Some(e) => {
+          assert(e.text.equals(message) === true)
+        }
+        case _ => failure("Entry option should be defined")
+      }
     }
 
     "load entries sorted from newest" in {
