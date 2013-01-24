3dd0fce7f059ddb87d7284f9b72d0f1f70d22bd1
Piotr Buda <pibuda@gmail.com>
2013-01-24 18:32:14 +0100

    Changed parameter name to better describe it

--- bootstrap-domain/src/main/scala/pl/softwaremill/bootstrap/domain/User.scala
+++ bootstrap-domain/src/main/scala/pl/softwaremill/bootstrap/domain/User.scala
@@ -8,8 +8,8 @@ case class User(_id: ObjectId = new ObjectId, login: String, loginLowerCased: St
 
 object User {
 
-  def apply(login: String, email: String, password: String, salt:String) = new User(login = login, loginLowerCased = login.toLowerCase,
-    email = email, password = encryptPassword(password, salt), salt = salt, token = generateToken(password, salt))
+  def apply(login: String, email: String, plainPassword: String, salt:String) = new User(login = login, loginLowerCased = login.toLowerCase,
+    email = email, password = encryptPassword(plainPassword, salt), salt = salt, token = generateToken(plainPassword, salt))
 
   def encryptPassword(password:String, salt:String) = {
     Utils.sha256(password, salt)
