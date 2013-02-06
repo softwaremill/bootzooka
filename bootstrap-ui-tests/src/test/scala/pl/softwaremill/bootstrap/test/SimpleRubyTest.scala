4456cde75198347b0bbe5e62e022cb2ad1cc08de
adamw <adam@warski.org>
2013-02-06 22:38:02 +0100

    One more test to migrate

--- bootstrap-ui-tests/src/test/scala/pl/softwaremill/bootstrap/test/SimpleRubyTest.scala
+++ bootstrap-ui-tests/src/test/scala/pl/softwaremill/bootstrap/test/SimpleRubyTest.scala
@@ -1,20 +1,16 @@
 package pl.softwaremill.bootstrap.test
 
-import org.specs2.mutable.Specification
 import org.jruby.embed.{PathType, ScriptingContainer}
 import org.jruby.RubyFixnum
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
 
-/**
- * .
- */
-class SimpleRubyTest extends Specification {
-  "This test" should {
-    "start jruby" in {
-      val container = new ScriptingContainer()
-      container.put("@filepath", "bootstrap-ui-tests/src/test/ruby/register_spec.rb")
-      val script = container.parse(PathType.RELATIVE, "bootstrap-ui-tests/src/test/ruby/simplescript.rb")
-      val executionResult:RubyFixnum = script.run().asInstanceOf[RubyFixnum]
-      executionResult.to_s.asJavaString() must be equalTo "0"
-    }
+class SimpleRubyTest extends FlatSpec with ShouldMatchers {
+  "This test" should "start jruby" in {
+    val container = new ScriptingContainer()
+    container.put("@filepath", "bootstrap-ui-tests/src/test/ruby/register_spec.rb")
+    val script = container.parse(PathType.RELATIVE, "bootstrap-ui-tests/src/test/ruby/simplescript.rb")
+    val executionResult:RubyFixnum = script.run().asInstanceOf[RubyFixnum]
+    executionResult.to_s.asJavaString() should be ("0")
   }
 }
