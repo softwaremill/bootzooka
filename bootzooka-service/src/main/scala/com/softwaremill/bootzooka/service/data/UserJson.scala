37839b41870f2b049e749e47bf4b6af98458c936
Jacek Kunicki <jacek@kunicki.org>
2013-05-09 15:50:26 +0200

    UserJson includes user id

--- bootzooka-service/src/main/scala/com/softwaremill/bootzooka/service/data/UserJson.scala
+++ bootzooka-service/src/main/scala/com/softwaremill/bootzooka/service/data/UserJson.scala
@@ -2,10 +2,10 @@ package com.softwaremill.bootzooka.service.data
 
 import com.softwaremill.bootzooka.domain.User
 
-case class UserJson(login: String, email:String, token: String)
+case class UserJson(id: String, login: String, email:String, token: String)
 
 object UserJson {
-  def apply(user: User) = new UserJson(user.login, user.email, user.token)
+  def apply(user: User) = new UserJson(user.id.toString, user.login, user.email, user.token)
 
   def apply(list: List[User]): List[UserJson] = {
     for (user <- list) yield UserJson(user)
