72229d7e1794d3833bd98fac4106f078e305e766
Maciej Bilas <maciej.bilas@gmail.com>
2013-07-04 15:54:19 +0200

    Increase memory available to the run.sh's sbt process.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -DwithInMemory=true -jar sbt-launch.jar container:start "~ compile"
+java -DwithInMemory=true -Xmx3000M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1024m -jar sbt-launch.jar container:start "~ compile"
