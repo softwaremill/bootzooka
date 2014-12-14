df5c5a362484e1323a03e6dead27112c90f35537
Jan Kowalski <jan.kowalski@softwaremill.com>
2014-12-14 19:53:29 +0100

    mongo instance in docker container

--- backend-start.sh
+++ backend-start.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
+
+docker 2>/dev/null
+if [ $? -ne 0 ]; then
+  echo "install docker first"
+  exit 1
+fi
+
+docker ps -a | grep bootzookaMongo
+if [ $? -ne 0 ]; then
+  docker run --name=bootzookaMongo -d -p 27017:27017 mongo:latest
+else
+ docker start bootzookaMongo
+fi
+
 sbt container:start "~ compile"
