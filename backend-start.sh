1b1e8d31c791dda68887c8155050ab9a641e8220
Jan Kowalski <jan.kowalski@softwaremill.com>
2014-12-28 12:34:19 +0100

    backend script with mongo in docker option

--- backend-start.sh
+++ backend-start.sh
@@ -1,16 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-docker 2>/dev/null
-if [ $? -ne 0 ]; then
-  echo "install docker first"
-  exit 1
-fi
+echo "This script might be run with mongo in docker container with: --docker"
+while [[ $1 = -* ]]; do
+    arg=$1; shift
 
-docker ps -a | grep bootzookaMongo
-if [ $? -ne 0 ]; then
-  docker run --name=bootzookaMongo -d -p 27017:27017 mongo:latest
-else
- docker start bootzookaMongo
-fi
+    case $arg in
+        --docker)
+            docker 2>/dev/null
+            if [ $? -ne 0 ]; then
+                echo "install docker first"
+                exit 1
+            fi
+            docker ps -a | grep bootzookaMongo
+            if [ $? -ne 0 ]; then
+                docker run --name=bootzookaMongo -d -p 27017:27017 mongo:latest
+            else
+                docker start bootzookaMongo
+            fi
+            ;;
+    esac
+done
 
 sbt container:start "~ compile"
