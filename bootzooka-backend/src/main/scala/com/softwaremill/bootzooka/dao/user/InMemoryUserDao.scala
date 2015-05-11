8a2c59dd1a9272867f6d5d527290470f8b13c50e
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2015-05-11 14:24:14 +0200

    Adjust InMemoryUserDao to return futures

--- bootzooka-backend/src/main/scala/com/softwaremill/bootzooka/dao/user/InMemoryUserDao.scala
+++ bootzooka-backend/src/main/scala/com/softwaremill/bootzooka/dao/user/InMemoryUserDao.scala
@@ -2,67 +2,83 @@ package com.softwaremill.bootzooka.dao.user
 
 import com.softwaremill.bootzooka.domain.User
 
-class InMemoryUserDao extends UserDao {
+import scala.concurrent.{ExecutionContext, Future}
+
+class InMemoryUserDao(implicit val ec: ExecutionContext) extends UserDao {
 
   var users = List[User]()
 
-  def loadAll: List[User] = {
-    users
+  override def loadAll(): Future[List[User]] = {
+    Future { users }
   }
 
-  override def findForIdentifiers(uniqueIds: Set[UserId]): List[User] = {
-    users.filter(user => uniqueIds.contains(user.id))
+  override def findForIdentifiers(uniqueIds: Set[UserId]): Future[List[User]] = {
+    Future { users.filter(user => uniqueIds.contains(user.id))}
   }
 
-  def countItems(): Long = {
-    users.size
+  override protected def internalAddUser(user: User) = {
+    Future { users ::= user }
   }
 
-  protected def internalAddUser(user: User) {
-    users ::= user
-  }
-
-  def remove(userId: UserId) {
-    load(userId) foreach { user =>
-      users = users.diff(List(user))
+  override def remove(userId: UserId) = {
+    Future {
+      load(userId) foreach { user =>
+        users = users.diff(List(user))
+      }
     }
   }
 
-  def load(userId: UserId): Option[User] = {
-    users.find(_.id == userId)
-  }
-
-  def findByEmail(email: String): Option[User] = {
-    users.find(user => user.email.toLowerCase == email.toLowerCase)
-  }
-
-  def findByLowerCasedLogin(login: String): Option[User] = {
-    users.find(user => user.loginLowerCased == login.toLowerCase)
-  }
-
-  def findByLoginOrEmail(loginOrEmail: String): Option[User] = {
-    findByEmail(loginOrEmail) orElse findByLowerCasedLogin(loginOrEmail)
-  }
-
-  def findByToken(token: String): Option[User] = {
-    users.find(user => user.token == token)
-  }
-
-  def changePassword(userId: UserId, password: String) {
-    load(userId) foreach { user =>
-      users = users.updated(users.indexOf(user), user.copy(password = password))
+  override def load(userId: UserId) = {
+    Future {
+      users.find(_.id == userId)
     }
   }
 
-  def changeLogin(currentLogin: String, newLogin: String) {
-    findByLowerCasedLogin(currentLogin) foreach { user =>
-      users = users.updated(users.indexOf(user), user.copy(login = newLogin, loginLowerCased = newLogin.toLowerCase))
+  override def findByEmail(email: String) = {
+    Future {
+      users.find(user => user.email.toLowerCase == email.toLowerCase)
     }
   }
 
-  def changeEmail(currentEmail: String, newEmail: String) {
-    findByEmail(currentEmail) foreach { user =>
-      users = users.updated(users.indexOf(user), user.copy(email = newEmail))
+  override def findByLowerCasedLogin(login: String) = {
+    Future {
+      users.find(user => user.loginLowerCased == login.toLowerCase)
+    }
+  }
+
+  override def findByLoginOrEmail(loginOrEmail: String) = {
+    findByLowerCasedLogin(loginOrEmail).flatMap(userOpt =>
+      userOpt.map(user => Future{Some(user)}).getOrElse(findByEmail(loginOrEmail))
+    )
+  }
+
+  def findByToken(token: String) = {
+    Future {
+      users.find(user => user.token == token)
+    }
+  }
+
+  def changePassword(userId: UserId, password: String) = {
+    load(userId).map { userOpt =>
+      userOpt.foreach { user =>
+        users = users.updated(users.indexOf(user), user.copy(password = password))
+      }
+    }
+  }
+
+  def changeLogin(currentLogin: String, newLogin: String) = {
+    findByLowerCasedLogin(currentLogin).map { userOpt =>
+      userOpt.foreach { user =>
+        users = users.updated(users.indexOf(user), user.copy(login = newLogin, loginLowerCased = newLogin.toLowerCase))
+      }
+    }
+  }
+
+  def changeEmail(currentEmail: String, newEmail: String) = {
+    findByEmail(currentEmail).map { userOpt =>
+      userOpt.foreach { user =>
+        users = users.updated(users.indexOf(user), user.copy(email = newEmail))
+      }
     }
   }
 }
