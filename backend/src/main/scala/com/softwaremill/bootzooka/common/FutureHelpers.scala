c5b0a83686ad119e43b6a67921ca69ce29564b8a
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2015-06-08 17:28:11 +0200

    [minor] simplify syntax to represent unit value

--- backend/src/main/scala/com/softwaremill/bootzooka/common/FutureHelpers.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/common/FutureHelpers.scala
@@ -4,6 +4,6 @@ import scala.concurrent.{ExecutionContext, Future}
 
 object FutureHelpers {
   implicit class PimpedFuture[T](future: Future[T])(implicit val ec: ExecutionContext) {
-    def mapToUnit = future.map(_ => (): Unit)
+    def mapToUnit = future.map(_ => ())
   }
 }
