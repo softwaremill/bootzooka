9a084d999003a9d5ef5bb01754f4770d0b0c3b28
Paweł Stawicki <pawelstawicki@gmail.com>
2015-06-18 16:27:03 +0200

    Change files pattern in html2js task.

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -82,7 +82,7 @@ module.exports = function (grunt) {
                 options: {
                     base: 'app'
                 },
-                src: ['app/**/*.html', 'app/common/**/*.html'],
+                src: ['app/**/*.html'],
                 dest: '.tmp/scripts/templates.js',
                 module: 'smlBootzooka.templates'
             }
