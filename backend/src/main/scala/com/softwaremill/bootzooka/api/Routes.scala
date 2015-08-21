4834d775cc19b685d768ac7bf28ad95f742d56a5
adamw <adam@warski.org>
2015-08-21 16:34:36 +0200

    Logging requests

--- backend/src/main/scala/com/softwaremill/bootzooka/api/Routes.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/api/Routes.scala
@@ -2,7 +2,7 @@ package com.softwaremill.bootzooka.api
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.ExceptionHandler
+import akka.http.scaladsl.server.{RejectionHandler, Route, ExceptionHandler}
 import com.softwaremill.bootzooka.passwordreset.PasswordResetRoutes
 import com.softwaremill.bootzooka.user.UsersRoutes
 
@@ -17,20 +17,33 @@ trait Routes extends UsersRoutes
       _.complete(StatusCodes.InternalServerError, "Internal server error")
   }
 
-  // TODO: request logger
-  val routes = handleExceptions(exceptionHandler) {
-    cacheImages {
-      encodeResponse {
-        pathPrefix("api") {
-          passwordResetRoutes ~
-            usersRoutes ~
-            versionRoutes
-        } ~
-          getFromResourceDirectory("webapp") ~
-          path("") {
-            getFromResource("webapp/index.html")
+  val routes =
+    logDuration {
+      handleExceptions(exceptionHandler) {
+        cacheImages {
+          encodeResponse {
+            pathPrefix("api") {
+              passwordResetRoutes ~
+                usersRoutes ~
+                versionRoutes
+            } ~
+              getFromResourceDirectory("webapp") ~
+              path("") {
+                getFromResource("webapp/index.html")
+              }
           }
+        }
       }
     }
+
+  def logDuration(inner: Route): Route = { ctx =>
+    val start = System.currentTimeMillis()
+    // handling rejections here so that we get proper status codes
+    val innerRejectionsHandled = handleRejections(RejectionHandler.default)(inner)
+    mapResponse { resp =>
+      val d = System.currentTimeMillis() - start
+      logger.info(s"[${resp.status.intValue()}] ${ctx.request.method.name} ${ctx.request.uri} took: ${d}ms")
+      resp
+    }(innerRejectionsHandled)(ctx)
   }
 }
