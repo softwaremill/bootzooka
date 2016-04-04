04eed20de56e0da87485a7cf6d1736323debf1e7
Jan Kowalski <jan.kowalski@softwaremill.com>
2016-04-04 14:17:36 +0200

    initial webpack config

---
+++ ui/webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+
+var config = {
+  context: __dirname + '/app',
+  entry: './index.js',
+  output: {
+    path: __dirname + '/app',
+    filename: 'bundle.js'
+  },
+
+  plugins: [],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'ng-annotate!babel'
+      },
+      {test: /.html$/, loader: 'raw', exclude: /(node_modules)/},
+      {test: /.css$/, loader: 'style!css'},
+      {test: /.styl$/, loader: 'style!css!stylus', exclude: /(node_modules)/},
+      {test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/, loader: 'file'}
+    ]
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.output.path = __dirname + '/dist';
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+module.exports = config;
