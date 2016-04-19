5c69bac82b52bcea6f2872f778c1d7cff0f9d4e7
Jan Kowalski <jan.kowalski@softwaremill.com>
2016-04-19 10:52:24 +0200

    imports to top

--- ui/app/index.js
+++ ui/app/index.js
@@ -1,29 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import './common/css/application.css';
 import angular from 'angular'
 import uirouter from 'angular-ui-router';
 import sanitize from 'angular-sanitize';
-import {intercept, stateChangeSuccess, stateChangeStart, routing} from './app.config'
 import ngResource from 'angular-resource'
 import ngCookies from 'angular-cookies'
 
-const commonModule = angular.module('smlBootzooka.common', [ngCookies]);
+import './common/css/application.css';
+import {intercept, stateChangeSuccess, stateChangeStart, routing} from './app.config'
 import common from './component/common';
+import profile from './component/profile';
+import bsTracker from './component/directives/bsHttpRequestTracker';
+import homeCtrl from './component/home';
+
+const commonModule = angular.module('smlBootzooka.common', [ngCookies]);
 common(commonModule);
 
 const profileModule = angular.module('smlBootzooka.profile', [uirouter, ngResource, ngCookies, 'smlBootzooka.common']);
-import profile from './component/profile';
 profile(profileModule);
 
 const bsTrackerModule = angular.module('smlBootzooka.bsTracker', []);
-import bsTracker from './component/directives/bsHttpRequestTracker';
 bsTracker(bsTrackerModule);
 
 const homeModule = angular.module('smlBootzooka.home', [uirouter]);
-import homeCtrl from './component/home';
 homeCtrl(homeModule);
 
-const ngModule = angular.module('smlBootzooka', 
+const ngModule = angular.module('smlBootzooka',
     [uirouter, sanitize, 'smlBootzooka.common', 'smlBootzooka.profile', 'smlBootzooka.bsTracker', 'smlBootzooka.home'])
   .config(routing)
   .config(intercept)
