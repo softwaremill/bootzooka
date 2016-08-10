081dbbf9024aa915d3c0fd4222af5048f58886d0
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2016-08-10 17:41:22 +0200

    Fix usage of execution contexts - Implicit EC provided by ScalaTest cannot be used outside tests, so we have to use our own here

--- backend/src/test/scala/com/softwaremill/bootzooka/test/TestHelpersWithDb.scala
+++ backend/src/test/scala/com/softwaremill/bootzooka/test/TestHelpersWithDb.scala
@@ -1,23 +1,24 @@
 package com.softwaremill.bootzooka.test
 
-import akka.dispatch.ExecutionContexts
 import com.softwaremill.bootzooka.common.sql.SqlDatabase
 import com.softwaremill.bootzooka.email.application.{DummyEmailService, EmailTemplatingEngine}
 import com.softwaremill.bootzooka.user.application.{UserDao, UserService}
 import com.softwaremill.bootzooka.user.domain.User
-import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
-
+import org.scalatest.AsyncFlatSpec
+import org.scalatest.concurrent.ScalaFutures
 import scala.concurrent.ExecutionContext
 
 trait TestHelpersWithDb extends TestHelpers with ScalaFutures {
 
+  this: AsyncFlatSpec =>
+
   lazy val emailService = new DummyEmailService()
   lazy val emailTemplatingEngine = new EmailTemplatingEngine
+  lazy val initEc = ExecutionContext.global
 
-  lazy val userDao = new UserDao(sqlDatabase)
-  lazy val userService = new UserService(userDao, emailService, emailTemplatingEngine)
+  lazy val userDao = new UserDao(sqlDatabase)(initEc)
+  lazy val userService = new UserService(userDao, emailService, emailTemplatingEngine)(initEc)
 
-  implicit def executionContext: ExecutionContext
   def sqlDatabase: SqlDatabase
 
   def newRandomStoredUser(password: Option[String] = None): User = {
