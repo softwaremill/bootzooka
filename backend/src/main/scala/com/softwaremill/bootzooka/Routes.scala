ea45d88b11ba483ef8b19b7e6ff7d06fa5e87f5a
Roman Melnyk <aywengo@gmail.com>
2017-04-15 18:03:31 +0200

    use object config from Cake instead of meaningless invocation of ConfigFactory.load() in trait

--- backend/src/main/scala/com/softwaremill/bootzooka/Routes.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/Routes.scala
@@ -7,15 +7,11 @@ import com.softwaremill.bootzooka.passwordreset.api.PasswordResetRoutes
 import com.softwaremill.bootzooka.swagger.SwaggerDocService
 import com.softwaremill.bootzooka.user.api.UsersRoutes
 import com.softwaremill.bootzooka.version.VersionRoutes
-import com.typesafe.config.ConfigFactory
 
 trait Routes extends RoutesRequestWrapper with UsersRoutes with PasswordResetRoutes with VersionRoutes {
 
   def system: ActorSystem
-
-  lazy val serverConfig = new ServerConfig {
-    override def rootConfig = ConfigFactory.load()
-  }
+  def config: ServerConfig
 
   lazy val routes = requestWrapper {
     pathPrefix("api") {
@@ -24,7 +20,7 @@ trait Routes extends RoutesRequestWrapper with UsersRoutes with PasswordResetRou
         versionRoutes
     } ~
       getFromResourceDirectory("webapp") ~
-      new SwaggerDocService(serverConfig.serverHost, serverConfig.serverPort, system).routes ~
+      new SwaggerDocService(config.serverHost, config.serverPort, system).routes ~
       path("") {
         getFromResource("webapp/index.html")
       }
