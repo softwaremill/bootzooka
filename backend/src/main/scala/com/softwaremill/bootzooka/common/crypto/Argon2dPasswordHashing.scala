c7e7ef2e0a1abe5c643b59a503d9c47beb642665
Piotr Buda <piotr.buda@softwaremill.com>
2017-10-06 08:26:05 +0200

    Documenting requiresRehashing for argon2

--- backend/src/main/scala/com/softwaremill/bootzooka/common/crypto/Argon2dPasswordHashing.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/common/crypto/Argon2dPasswordHashing.scala
@@ -14,6 +14,8 @@ class Argon2dPasswordHashing(config: CryptoConfig) extends PasswordHashing with 
     argon2.verify(hash, salt + password)
 
   def requiresRehashing(hash: String): Boolean = {
+    //argon2 stores hash in the form $argon2$v=19$m=1024,t=2,p=4$hashvalueprobablywithsalt
+    //so split here needs to take fourth value (first is before first $)
     val hashParams   = hash.split('$')(3)
     val configParams = s"m=${config.memory},t=${config.iterations},p=${config.parallelism}"
     configParams != hashParams
