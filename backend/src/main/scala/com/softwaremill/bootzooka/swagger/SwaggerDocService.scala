3538dee5b429b6c8e7fe6efd3d3ecb5ec287f085
adamw <adam@warski.org>
2017-10-23 18:41:15 +0200

    Fixing compilation for 2.11

--- backend/src/main/scala/com/softwaremill/bootzooka/swagger/SwaggerDocService.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/swagger/SwaggerDocService.scala
@@ -8,7 +8,7 @@ import com.softwaremill.bootzooka.version.VersionRoutes
 import com.softwaremill.bootzooka.version.BuildInfo._
 
 class SwaggerDocService(address: String, port: Int, system: ActorSystem) extends SwaggerHttpService {
-  override val apiClasses = Set( // add here routes in order to add to swagger
+  override val apiClasses: Set[Class[_]] = Set( // add here routes in order to add to swagger
     classOf[VersionRoutes]
   )
   override val host        = address + ":" + port
