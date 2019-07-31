46df97cee16d7c0d620dc97467cb01b77c34b6d4
adamw <adam@warski.org>
2019-07-31 12:31:46 +0200

    Validate password reset config on startup

--- backend/src/main/scala/com/softwaremill/bootzooka/passwordreset/PasswordResetConfig.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/passwordreset/PasswordResetConfig.scala
@@ -2,4 +2,13 @@ package com.softwaremill.bootzooka.passwordreset
 
 import scala.concurrent.duration.Duration
 
-case class PasswordResetConfig(resetLinkPattern: String, codeValid: Duration)
+case class PasswordResetConfig(resetLinkPattern: String, codeValid: Duration) {
+  validate()
+
+  def validate(): Unit = {
+    val testCode = "TEST_123"
+    if (!String.format(resetLinkPattern, testCode).contains(testCode)) {
+      throw new IllegalStateException(s"Invalid reset link pattern: $resetLinkPattern. Formatting with a test code didn't contain the code in the result.")
+    }
+  }
+}
