4d16e6845efea39699e1881b99db07b5803ac432
bubu <michal.mital@softwaremill.com>
2020-04-06 10:32:02 +0200

    Defined post & get as functions. Removed class service

--- ui-typescript/src/services/api.ts
+++ ui-typescript/src/services/api.ts
@@ -1,25 +1,25 @@
 import axios, { AxiosError, AxiosRequestConfig } from "axios";
 import { Either, Left, Right } from "ts-matches";
 
-class Api {
-  constructor(readonly apiUrl: string) {
-  }
+const apiUrl: string = process.env.REACT_API_URL || '';
 
-  public async get<T, Err = any>(url: string, config: AxiosRequestConfig = {}): Promise<Either<AxiosError<Err>, T>> {
-    try {
-      const { data } = await axios.get(url, config);
-      return Right.of(data);
-    } catch (error) {
-      return Left.of(error)
-    }
+const get = async <T, Err = any>(url: string, config: AxiosRequestConfig = {}): Promise<Either<AxiosError<Err>, T>> => {
+  try {
+    const { data } = await axios.get(url, config);
+    return Right.of(data);
+  } catch (error) {
+    return Left.of(error)
   }
+};
 
-  public async post<V, T, Err = any>(url: string, data: V, config: AxiosRequestConfig): Promise<Either<AxiosError<Err>, T>> {
-    try {
-      const { response } = await axios.post(url, data, config);
-      return Right.of(response);
-    } catch (error) {
-      return Left.of(error);
-    }
+const post = async <V, T, Err = any>(url: string, data: V, config: AxiosRequestConfig): Promise<Either<AxiosError<Err>, T>> => {
+  try {
+    const { response } = await axios.post(url, data, config);
+    return Right.of(response);
+  } catch (error) {
+    return Left.of(error);
   }
-}
+};
+
+
+export { get, post }
