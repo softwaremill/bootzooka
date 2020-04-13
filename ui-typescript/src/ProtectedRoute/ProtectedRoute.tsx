3ea897bee4073db8f3cbc00b0131fcad18fdf192
bubu <michal.mital@softwaremill.com>
2020-04-13 11:16:05 +0200

    Added props type with indexable type

--- ui-typescript/src/ProtectedRoute/ProtectedRoute.tsx
+++ ui-typescript/src/ProtectedRoute/ProtectedRoute.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const ProtectedRoute: React.FC = ({ isLoggedIn, ...rest }: any) =>
+type Props = {
+  isLoggedIn: boolean;
+  [key: string]: any
+}
+
+const ProtectedRoute: React.FC<Props> = ({ isLoggedIn, ...rest }) =>
   isLoggedIn ? <Route {...rest} /> : <Redirect to="/login" />;
 
 export default ProtectedRoute;
