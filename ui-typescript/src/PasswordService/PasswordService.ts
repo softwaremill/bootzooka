4b1961aee70155b1e1e4038e7f56f5e1c31a90a4
bubu <michal.mital@softwaremill.com>
2020-04-13 21:21:59 +0200

    Added password api

--- ui-typescript/src/PasswordService/PasswordService.ts
+++ ui-typescript/src/PasswordService/PasswordService.ts
@@ -1,16 +1,15 @@
-import axios from 'axios';
 import { Either } from "ts-matches";
-import { post } from "../Services/api";
+import { post } from "../Api/api";
 
 class PasswordService {
   static context = 'api/v1/passwordreset';
 
   // TODO extract to a separate service, add unit tests
-  static claimPasswordRest(loginOrEmail: string): Promise<Either<Error, string>> {
+  static claimPasswordReset(loginOrEmail: string): Promise<Either<Error, string>> {
     return post(`${PasswordService.context}/forgot`, { loginOrEmail });
   }
   static resetPassword(code: string, password: string) {
-    return axios.post(`${PasswordService.context}/reset`, { code, password });
+    return post(`${PasswordService.context}/reset`, { code, password });
   }
 }
 
