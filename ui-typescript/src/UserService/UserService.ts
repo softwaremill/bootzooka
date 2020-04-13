11697c15130a9c584b2b89f99bca90e28448aca7
bubu <michal.mital@softwaremill.com>
2020-04-13 21:20:38 +0200

    Simplified user api

--- ui-typescript/src/UserService/UserService.ts
+++ ui-typescript/src/UserService/UserService.ts
@@ -1,51 +1,25 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { ChangePasswordData, LoginData, UserRegistrationData } from "../types/Types";
+import { ChangePasswordData, LoginData, User, UserRegistrationData } from "../types/Types";
+import { post, securedPost, securedGet, get } from "../Api/api";
 
-const userUrl = 'api/v1/user';
+const userUrl = '/api/v1/user';
 
 export const registerUser = ({ login, email, password }: UserRegistrationData) => {
-  return axios.post(`${userUrl}/register`, { login, email, password });
+  return post(`${userUrl}/register`, { login, email, password });
 };
 
 export const login = ({ loginOrEmail, password }: LoginData) => {
-  return axios.post(`${userUrl}/login`, { loginOrEmail, password, apiKeyValidHours: 1 });
+  return post(`${userUrl}/login`, { loginOrEmail, password, apiKeyValidHours: 1 });
 };
 
 export const getCurrentUser = (apiKey: string) => {
-  return _securedRequest(apiKey, {
-    method: 'GET',
-    url: userUrl
-  });
+  return securedGet(apiKey, userUrl);
 };
 
-export const changeProfileDetails = (apiKey: string, { email, login }: { email: string, login: string }) => {
-  return _securedRequest(apiKey, {
-    method: 'POST',
-    url: userUrl,
-    data: {
-      email,
-      login
-    }
-  });
+export const changeProfileDetails = (apiKey: string, { email, login }: User) => {
+  return securedPost(apiKey, userUrl, { email, login });
 };
 
 export const changePassword = (apiKey: string, { currentPassword, newPassword }: ChangePasswordData) => {
-  return _securedRequest(apiKey, {
-    method: 'POST',
-    url: `${userUrl}/changepassword`,
-    data: {
-      currentPassword,
-      newPassword
-    }
-  });
-};
-
-const _securedRequest = (apiKey: string, config?: AxiosRequestConfig) => {
-  return axios.request({
-    headers: {
-      Authorization: `Bearer ${apiKey}`
-    },
-    ...config
-  });
+  return securedPost(apiKey, `${userUrl}/changepassword`, { currentPassword, newPassword });
 };
 
