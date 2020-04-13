64a8390ddb4de3d20103a57416bd18d6771a8fb8
bubu <michal.mital@softwaremill.com>
2020-04-13 20:11:05 +0200

    Fixed version service

--- ui-typescript/src/Api/api.ts
+++ ui-typescript/src/Api/api.ts
@@ -5,17 +5,17 @@ const apiUrl: string = process.env.REACT_API_URL || '';
 
 const get = async <T, Err = any>(url: string, config: AxiosRequestConfig = {}): Promise<Either<AxiosError<Err>, T>> => {
   try {
-    const { data } = await axios.get(url, config);
+    const { data } = await axios.get(`${apiUrl}${url}`, config);
     return Right.of(data);
   } catch (error) {
-    return Left.of(error)
+    return Left.of(error);
   }
 };
 
-const post = async <V, T, Err = any>(url: string, data: V, config: AxiosRequestConfig = {}): Promise<Either<AxiosError<Err>, T>> => {
+const post = async <V, T, Err = any>(url: string, requestData: V, config: AxiosRequestConfig = {}): Promise<Either<Err, T>> => {
   try {
-    const { response } = await axios.post(url, data, config);
-    return Right.of(response);
+    const { data } = await axios.post(`${apiUrl}${url}`, requestData, config);
+    return Right.of(data);
   } catch (error) {
     return Left.of(error);
   }
--- ui-typescript/src/VersionService/VersionService.ts
+++ ui-typescript/src/VersionService/VersionService.ts
@@ -1,3 +1,4 @@
 import { get } from '../Api/api';
+import { Version } from "../types/Types";
 
-export const getAppVersion = async () => get("/admin/version");
+export const getAppVersion = async () => await get<Version>("/admin/version");
