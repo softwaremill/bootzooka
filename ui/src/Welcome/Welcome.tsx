84d5eef3c71a87771135bf0842c8cfd9f3b3ac80
Tomasz Krawczyk <grubach1@gmail.com>
2020-10-12 13:50:39 +0200

    no spaces image file name

--- ui/src/Welcome/Welcome.tsx
+++ ui/src/Welcome/Welcome.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./sml logo vertical rgb trans.png";
+import logo from "./sml-logo-vertical-rgb-trans.png";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
