a63c406d161ce87fcea27eab75e0900e0b3bc18f
Tomasz Krawczyk <grubach1@gmail.com>
2020-10-12 13:02:45 +0200

    welcome screen tweak

--- ui/src/Welcome/Welcome.tsx
+++ ui/src/Welcome/Welcome.tsx
@@ -4,26 +4,30 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 
 const Welcome: React.FC = () => (
-  <Container className="py-5">
-    <h3>Hi there! Welcome to Bootzooka!</h3>
-    <p>In this template application you can register as a new user, log in and later manage your user details.</p>
-    <p>
-      If you are interested in how Bootzooka works, you can{" "}
-      <a href="http://softwaremill.github.io/bootzooka/" target="blank">
-        browse the documentation
-      </a>
-      , or the{" "}
-      <a href="https://github.com/softwaremill/bootzooka" target="blank">
-        source code
-      </a>
-      .
-    </p>
+  <>
+    <Container fluid className="py-5 bg-primary text-light text-center">
+      <h3>Hi there! Welcome to Bootzooka!</h3>
+      <p>In this template application you can register as a new user, log in and later manage your user details.</p>
+    </Container>
+    <Container className="py-5">
+      <p>
+        If you are interested in how Bootzooka works, you can{" "}
+        <a href="http://softwaremill.github.io/bootzooka/" target="blank">
+          browse the documentation
+        </a>
+        , or the{" "}
+        <a href="https://github.com/softwaremill/bootzooka" target="blank">
+          source code
+        </a>
+        .
+      </p>
 
-    <small>brought to you by</small>
-    <a href="http://softwaremill.com" rel="noopener noreferrer" target="_blank">
-      {/* <Image className="sm-3" fluid src={logo} alt="SoftwareMill" /> */}
-    </a>
-  </Container>
+      <small>brought to you by</small>
+      <a href="http://softwaremill.com" rel="noopener noreferrer" target="_blank">
+        {/* <Image className="sm-3" fluid src={logo} alt="SoftwareMill" /> */}
+      </a>
+    </Container>
+  </>
 );
 
 export default Welcome;
