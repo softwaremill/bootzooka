7e593b63f4bb952a19599c1c4d25537eb7b16d9c
Tomasz Krawczyk <grubach1@gmail.com>
2020-10-15 18:05:38 +0200

    top navbar tweaks

--- ui/src/layout/Top/Top.tsx
+++ ui/src/layout/Top/Top.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
 import { UserContext } from "../../contexts/UserContext/UserContext";
 import { BiPowerOff, BiHappy } from "react-icons/bi";
@@ -15,44 +16,46 @@ const Top: React.FC = () => {
 
   return (
     <Navbar variant="dark" bg="dark" sticky="top" collapseOnSelect expand="lg">
-      <LinkContainer exact to="/">
-        <Navbar.Brand>Bootzooka</Navbar.Brand>
-      </LinkContainer>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="d-flex flex-grow-1 justify-content-between">
-          <LinkContainer exact to="/">
-            <Nav.Link>Welcome</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/main">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <div className="flex-grow-1" />
-          {loggedIn ? (
-            <>
-              <LinkContainer to="/profile">
-                <Nav.Link className="text-right">
-                  <BiHappy />
-                  &nbsp;{user?.login}
+      <Container>
+        <LinkContainer exact to="/">
+          <Navbar.Brand>Bootzooka</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="d-flex flex-grow-1 justify-content-between">
+            <LinkContainer exact to="/">
+              <Nav.Link>Welcome</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/main">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <div className="flex-grow-1" />
+            {loggedIn ? (
+              <>
+                <LinkContainer to="/profile">
+                  <Nav.Link className="text-right">
+                    <BiHappy />
+                    &nbsp;{user?.login}
+                  </Nav.Link>
+                </LinkContainer>{" "}
+                <Nav.Link className="text-right" onClick={handleLogOut}>
+                  <BiPowerOff />
+                  &nbsp;Logout
                 </Nav.Link>
-              </LinkContainer>{" "}
-              <Nav.Link className="text-right" onClick={handleLogOut}>
-                <BiPowerOff />
-                &nbsp;Logout
-              </Nav.Link>
-            </>
-          ) : (
-            <>
-              <LinkContainer to="/register">
-                <Nav.Link>Register</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>Login</Nav.Link>
-              </LinkContainer>
-            </>
-          )}
-        </Nav>
-      </Navbar.Collapse>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/register">
+                  <Nav.Link>Register</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
