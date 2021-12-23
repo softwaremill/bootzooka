1de57283becdfd34b70d18195cea2863d1ef0f70
adamw <adam@warski.org>
2021-12-23 11:08:32 +0100

    Fix compile

--- backend/src/test/scala/com/softwaremill/bootzooka/email/sender/DummyEmailSenderTest.scala
+++ backend/src/test/scala/com/softwaremill/bootzooka/email/sender/DummyEmailSenderTest.scala
@@ -3,6 +3,8 @@ package com.softwaremill.bootzooka.email.sender
 import com.softwaremill.bootzooka.email.EmailData
 import com.softwaremill.bootzooka.test.BaseTest
 
+import cats.effect.unsafe.implicits.global
+
 class DummyEmailSenderTest extends BaseTest {
   it should "send scheduled email" in {
     DummyEmailSender(EmailData("test@sml.com", "subject", "content")).unsafeRunSync()
