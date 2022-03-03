8e333773ec924226f3b4726035efa82425ad7ed0
Pask <pask423th@gmail.com>
2022-03-03 21:51:07 +0100

    Fix swaggerUI path

--- backend/src/main/scala/com/softwaremill/bootzooka/http/EndpointsToRoutes.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/http/EndpointsToRoutes.scala
@@ -5,6 +5,7 @@ import com.softwaremill.bootzooka.util.ServerEndpoints
 import org.http4s.HttpRoutes
 import sttp.tapir.server.http4s._
 import sttp.tapir.server.interceptor.ValuedEndpointOutput
+import sttp.tapir.swagger.SwaggerUIOptions
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
 
 class EndpointsToRoutes(http: Http, apiContextPath: List[String]) {
@@ -21,7 +22,7 @@ class EndpointsToRoutes(http: Http, apiContextPath: List[String]) {
   /** Interprets the given endpoint descriptions as docs, and returns http4s routes which expose the documentation using Swagger. */
   def toDocsRoutes(es: ServerEndpoints): HttpRoutes[IO] = {
     val swaggerEndpoints =
-      SwaggerInterpreter(contextPath = apiContextPath, customiseDocsModel = _.addServer(s"/${apiContextPath.mkString("/")}"))
+      SwaggerInterpreter(swaggerUIOptions = SwaggerUIOptions.default.copy(contextPath = apiContextPath), customiseDocsModel = _.addServer(s"/${apiContextPath.mkString("/")}"))
         .fromServerEndpoints(es.toList, "My App", "1.0")
     Http4sServerInterpreter[IO]().toRoutes(swaggerEndpoints)
   }
