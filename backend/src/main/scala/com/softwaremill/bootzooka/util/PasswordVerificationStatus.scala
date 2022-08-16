abfefeed0929e5b5aeacc1077869a0ec143a9f95
Rafal Ambrozewicz <rafal.ambrozewicz@gmail.com>
2022-08-16 08:35:54 +0200

    PR changes

--- backend/src/main/scala/com/softwaremill/bootzooka/util/PasswordVerificationStatus.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/util/PasswordVerificationStatus.scala
@@ -1,5 +1,5 @@
 package com.softwaremill.bootzooka.util
 
-trait PasswordVerificationStatus
+sealed trait PasswordVerificationStatus
 case object Verified           extends PasswordVerificationStatus
 case object VerificationFailed extends PasswordVerificationStatus
