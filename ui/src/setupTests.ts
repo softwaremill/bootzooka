3e7ee997056dda05d3303aeb0a60d426a567f69b
Mateusz Dybowski <matdybowski@gmail.com>
2024-01-08 23:48:15 +0100

    Add utility functions for mocking and delaying resolved/rejected values

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+const defaultDelay = 50;
+
+export const mockAndDelayResolvedValueOnce = (mock: jest.Mock, value: any, delay: number = defaultDelay) =>
+  mock.mockImplementationOnce(() => new Promise((resolve) => setTimeout(() => resolve(value), delay)));
+
+export const mockAndDelayRejectedValueOnce = (mock: jest.Mock, value: any, delay: number = defaultDelay) =>
+  mock.mockImplementationOnce(() => new Promise((_, reject) => setTimeout(() => reject(value), delay)));
