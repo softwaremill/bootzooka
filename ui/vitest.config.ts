17e4f2cffe74dde2de73a329bbe830a5b23d6f11
Bartosz Butrym <bartosz.butrym@softwaremill.pl>
2025-01-20 15:58:45 +0100

    adjust vitest config file

--- ui/vitest.config.ts
+++ ui/vitest.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./src/setupTest.ts'],
-      reporters: ['verbose'],
-      coverage: {
-        reporter: ['text', 'json', 'html'],
-        include: ['src/**/*'],
-        exclude: [],
+export default defineConfig((env) =>
+  mergeConfig(
+    viteConfig(env),
+    defineConfig({
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/setupTest.ts'],
+        reporters: ['verbose'],
+        coverage: {
+          reporter: ['text', 'json', 'html'],
+          include: ['src/**/*'],
+          exclude: [],
+        },
       },
-    },
-  })
+    })
+  )
 );
