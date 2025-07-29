39949357f742d6fb71b6152460f5cc0ad9f7a362
afternoon2 <poprostuantolak@gmail.com>
2025-07-29 16:17:37 +0200

    Update index.ts

--- ui/src/main/index.ts
+++ ui/src/main/index.ts
@@ -1,6 +1,5 @@
 export * from './Footer/Footer';
 export * from './ForkMe/ForkMe';
-export * from './Loader/Loader';
 export * from './Main/Main';
 export * from './Routes/Routes';
 export * from './Top/Top';
